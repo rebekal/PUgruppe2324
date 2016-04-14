71773a010bab1d744880276a37e46f0117cf8a35
christianodden <christian_odden@hotmail.com>
2016-04-14 13:58:59 +0200

    Added files via upload

---
+++ display.java
@@ -0,0 +1,21 @@
+package software;
+
+public class display {
+	public static void main(String[] args) {
+		String disp = 
+			   "------------------------------------- \n"
+			+  "**          Parking Mode           ** \n"
+			+  "------------------------------------- \n"
+			+  "                                      \n"
+			+  "              Front dist              \n"
+			+  "               -*----*-               \n"
+			+  "              |        |              \n"
+			+  "              |________|              \n"
+			+  "     Left     |        |     Right    \n"
+			+  "       	      |        |		      \n"
+			+  "              |________|              \n"
+			+  "              |        |              \n"
+			+  "              |________|              \n";
+		System.out.println(disp);
+	}
+}
